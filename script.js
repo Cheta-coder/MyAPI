8ca9f82689ff75b2681b3ac32031a2c760902e5f
Cheta-coder <happinessugwuanyi7@gmail.com>
2023-11-28 16:40:23 +0100

    another commit

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-let display = document.getElementById('display')
-let jokeSeries = [];
-// Asynchronous Programming
+let displayType = document.getElementById("displayType")
+let jokeline = document.getElementById("displayJoke")
+let JQuestion = document.getElementById("displayQuestion")
 
-// JS Promises
-let getJokes = () => {
-    // const URL = 'http://universities.hipolabs.com/search?countru=United+States'
-    const URL = 'http://sv443.net/jokeapi/v2'
-    let myJokes = fetch(URL);
-    myJokes.then((data) => data.json())
-    .then((data) => {
-        jokeSeries = data;
-        console.log(data[0])
-    })
-    .catch(err => console.log(err));
+let req = fetch ('https://official-joke-api.appspot.com/random_joke')
+req.then(cheta => cheta.json())
+.then(res => {
+    console.log(res)
+
+    displayType.innerHTML = res.type
+    JQuestion.innerHTML = res.setup
+    jokeline.innerHTML = res.punchline
+})
+.catch(err =>{throw "I can't fit"})
+.catch(err => console.log(err))
+
+// Using async function to write promises
+let cheta = async() =>{
+    try{
+        let seen = await fetch('https://official-joke-api.appspot.com/random_joke')
+        let reqStatus = seen.status;
+        if(reqStatus === 100){throw 'failure'}
+
+        let res = await seen.json()
+        console.log(data)
+        jokeline.innerHTML = res.punchline
+    }
+    catch(err){
+        console.log(err)
+    }
 }
-getJokes()
-
-setTimeout(() => {
-    console.log(jokeSeries);
-},10000)
-
-display.innerHTML = jokeSeries
-
-//Asyncawait
+cheta()
